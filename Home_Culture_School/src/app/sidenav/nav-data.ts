e391fa5c8ad3fb06dbee7731524a4db491cc1e38
Rafael Soares <rafael19euro@gmail.com>
2024-03-21 22:11:40 -0300

    Added barra lateral

---
+++ Home_Culture_School/src/app/sidenav/nav-data.ts
@@ -0,0 +1,37 @@
+export const navbarData = [
+  {
+      routeLink: 'dashboard',
+      icon: 'fal fa-home',
+      label: 'Cursos'
+  },
+  {
+      routeLink: 'products',
+      icon: 'fal fa-box-open',
+      label: 'Exercícios'
+  },
+  {
+      routeLink: 'statistics',
+      icon: 'fal fa-video',
+      label: 'Vídeos'
+  },
+  {
+      routeLink: 'coupens',
+      icon: 'fal fa-volume',
+      label: 'Audios'
+  },
+  {
+      routeLink: 'pages',
+      icon: 'fal fa-camera',
+      label: 'Fotos'
+  },
+  {
+      routeLink: 'media',
+      icon: 'fal fa-book',
+      label: 'Ebooks Free'
+  },
+  {
+      routeLink: 'settings',
+      icon: 'fal fa-address-card',
+      label: 'Cadastros'
+  },
+];
